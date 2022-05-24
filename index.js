54371c27615ca4896a856237ce7cd977553074e4
daviddiazh <daviddiazlds1221@gmail.com>
2022-05-23 19:19:50 -0500

    BUG-FIX: Added cors to index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const orderRoutes = require('./routes/order.routes');
-
+const cors = require('cors');
 const app = express();
-// const port = process.env.PORT = 5000;
 
 //Middleware
 app.use(express.json());
+app.use(cors());
 app.use('/api', orderRoutes);
 
 // Routes
