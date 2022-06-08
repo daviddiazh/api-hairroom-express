184c20ea664969458e9cd6f668c328f2248e546b
Dayro Martínez <dayro.martinez@sofka.com.co>
2022-06-07 22:57:04 -0500

    Cambio de requerimiento para el atributo email

--- models/Order.js
+++ models/Order.js
@@ -4,7 +4,7 @@ const orderSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: false },
     lastName: { type: String, required: true, trim: true },
     phone: { type: Number, required: true, trim: true },
-    email: { type: String, required: true, trim: true },
+    email: { type: String, required: false, trim: true },
     numberIdentification: { type: String, required: true, trim: true },
     typeIdentification: {
         type: String,
