d3709190535831327f0267edc0752e9a5a2b4c47
david.diazh <david.diazh@sofka.com.co>
2022-06-21 23:47:25 -0500

    final final no va mas

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
 
-app.listen(process.env.PORT || 5000, () => console.log('Server is running on port: ', process.env.PORT));
+app.listen(port, () => console.log('Server is running on port: ', port));
