419cd8bb60e4fca4737750a551f7b308703592a3
daviddiazh <daviddiazlds1221@gmail.com>
2022-07-08 18:58:03 -0500

    Adding template of env

--- config/index.js
+++ config/index.js
@@ -5,8 +5,6 @@ const config = {
     dbUrl: process.env.MONGODB_URI,
     apiKey: process.env.API_KEY,
     jwtSecret: process.env.JWT_SECRET,
-    fromEmailEmail: process.env.FROM_EMAIL_EMAIL,
-    fromEmailPassword: process.env.FROM_EMAIL_PASSWORD,
 }
 
 module.exports = { config };
